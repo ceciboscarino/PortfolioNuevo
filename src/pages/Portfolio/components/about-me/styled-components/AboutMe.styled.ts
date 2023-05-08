faff515d88c0c895212f16c2c4a698c3a2582f5a
ceciboscarino <ceciboscarino@hotmail.com>
2023-05-08 19:06:41 -0300

    Avances

--- src/pages/Portfolio/components/about-me/styled-components/AboutMe.styled.ts
+++ src/pages/Portfolio/components/about-me/styled-components/AboutMe.styled.ts
@@ -2,18 +2,28 @@ import styled from 'styled-components';
 
 export const AboutMeContainer = styled.div`
 
-padding-top: 100px;
+background-color: rgb(204,204,204);
+height: 600px;
+
+h1{
+    font-family: "Red Hat Mono Bold";
+    font-size: 10rem;
+    color: white;
+}
+
 
 
 div{
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
-    flex-wrap: wrap-reverse;
+    
     display : flex;
     flex-direction: row;
-    height: 900px;
+    z-index: 1;
     
+
+
     
     
 }
@@ -25,6 +35,7 @@ export const  AboutMeImg = styled.img`
 
 width: 40%;
 height: auto;
+
 
 
 `;
--- src/pages/Portfolio/components/contact/styled-components/Contact.styled.ts
+++ src/pages/Portfolio/components/contact/styled-components/Contact.styled.ts
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 export const ContactContainer = styled.div`
 
-height: 900px;
+height: 600px;
 padding-top: 80px;
+
+h1{
+    font-family: "Red Hat Mono Bold";
+    font-size: 10rem;
+}
 `;
